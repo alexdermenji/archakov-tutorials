abd70b006f333b008921e65a9b1c4209bfd232d6
alexdermenji <allexdermenji@gmail.com>
2021-12-04 10:50:11 +0000

    feedback app

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './tutorial4/App.jsx';
+import App from './tutorial5/App.jsx';
 
 ReactDOM.render(
   <React.StrictMode>
