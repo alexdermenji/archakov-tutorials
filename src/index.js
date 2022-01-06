5498b49d48a10c1625cf8bbb2f4f6704904952d5
alexdermenji <allexdermenji@gmail.com>
2022-01-06 19:08:36 +0000

    github search

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 import './index.css';
-import App from './tutorial8/App.jsx';
+import App from './tutorial9/App.jsx';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
